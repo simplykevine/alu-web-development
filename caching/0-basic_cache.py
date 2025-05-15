89b3b181226c34b3207375ab7e818fddc7dfeedf
UMUTONI Kevine <138827957+simplykevine@users.noreply.github.com>
2025-05-15 14:47:16 +0200

    caching

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    BasicCache class that inherits from BaseCaching
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Define Basic Cache """
+
+    def put(self, key, item):
+        """ Add key-value pair to the cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve value from the cache based on given key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
